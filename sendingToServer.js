56045912122a9db12cc5d269c4b5a08524ef5edf
mazation <mazation96@gmail.com>
2019-09-05 18:28:38 +0500

    Added function for auth

--- sendingToServer.js
+++ sendingToServer.js
@@ -5,11 +5,31 @@ regBtn = document.querySelector('#regBtn')
 
 const url = 'localhost:5000'
 
-formGroupLogin.onsubmit = async(e) => {
+formGroupReg.onsubmit = async(e) => {
   e.preventDefault();
-  console.log(FormGroupLogin.email)
-  // headers.set('Authorization', 'Basic ' + Buffer.from(username + ":" + password).toString('base64'));
-  // let response = await fetch(url + '/api/dashboard', {
-  //   headers: headers
-  // });
+  var object = {};
+  var formData = new FormData(document.forms.reg);
+
+  formData.forEach(function(value, key){
+      object[key] = value;
+  });
+  var json = JSON.stringify(object);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", 'localhost:5000/users', true)
+  xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+  // Отсылаем объект в формате JSON и с Content-Type application/json
+  xhr.send(json);
 }
+
+formGroupLogin.onsubmit = async(e) => {
+  e.preventDefault()
+  const formData = new FormData(document.forms.reg)
+  const authString = formData.email + ":" + formData.password
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "localhost:5000/dashboard", true)
+  xhr.setRequestHeader("Authorization", "Basic" + authString)
+}
+
+
