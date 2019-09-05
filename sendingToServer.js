83e99f1687b040f63879ef68de4aded8de20415c
mazation <mazation96@gmail.com>
2019-09-05 18:57:19 +0500

    smt

--- sendingToServer.js
+++ sendingToServer.js
@@ -5,31 +5,43 @@ regBtn = document.querySelector('#regBtn')
 
 const url = 'localhost:5000'
 
-formGroupReg.onsubmit = async(e) => {
-  e.preventDefault();
-  var object = {};
-  var formData = new FormData(document.forms.reg);
+// formGroupReg.onsubmit = async(e) => {
+//   e.preventDefault();
+//   var object = {};
+//   var formData = new FormData(document.forms.reg);
 
-  formData.forEach(function(value, key){
-      object[key] = value;
-  });
-  var json = JSON.stringify(object);
+//   formData.forEach(function(value, key){
+//       object[key] = value;
+//   });
+//   var json = JSON.stringify(object);
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", 'localhost:5000/users', true)
-  xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("POST", 'localhost:5000/users', true)
+//   xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
-  // Отсылаем объект в формате JSON и с Content-Type application/json
-  xhr.send(json);
-}
+//   // Отсылаем объект в формате JSON и с Content-Type application/json
+//   xhr.send(json);
+// }
 
-formGroupLogin.onsubmit = async(e) => {
+// formGroupLogin.onsubmit = async(e) => {
+//   e.preventDefault()
+//   console.log("HELLO")
+//   const formData = new FormData(document.forms.reg)
+//   const authString = formData.email + ":" + formData.password
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("GET", "localhost:5000/dashboard", true)
+//   xhr.setRequestHeader("Authorization", "Basic" + btoa(authString))
+// }
+formGroupLogin.addEventListener("submit", function(e){
   e.preventDefault()
-  const formData = new FormData(document.forms.reg)
-  const authString = formData.email + ":" + formData.password
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "localhost:5000/dashboard", true)
-  xhr.setRequestHeader("Authorization", "Basic" + authString)
-}
+  console.log("HELOOOOOOOOOOOO")
+})
+
+btn = document.getElementById("loginBtn")
+btn.addEventListener("click", function(e){
+  e.preventDefault;
+  console.log("HELOOOOOOOOOOOO")
+})
+console.log(document.forms)
 
 
